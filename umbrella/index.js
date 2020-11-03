cee352f8066ed5579a60f71dd35ea389f0379300
Sunfur Thanos <hormigence123@gmail.com>
2020-11-03 14:11:42 -0400

    Update index.js

--- umbrella/index.js
+++ umbrella/index.js
@@ -138,7 +138,7 @@ umbrella.testTime_conection_backup = umbrella.testTime_conection // !not-change!
 function func_isConectionServer() {
 
     var http = new XMLHttpRequest();
-    http.timeout = 1024
+    http.timeout  = umbrella.testTime_conection_backup
     var randomNum = Math.round(Math.random() * 10000);
 
     http.open('HEAD', document.location + "?rand=" + randomNum, true);
